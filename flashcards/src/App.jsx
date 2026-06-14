d814a4d34b4d66cf36872996e424fa6670bba66a
Ryan O'Malley <cro5058@g.rit.edu>
2026-06-14 17:00:14 -0400

    A single card at a time is displayed; Clicking the next button displays a random new card.

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
-import { useState } from 'react'
-import cardData from './card_data.jsx'
+import './App.css';
+import { useState } from 'react';
+import Card from './components/Card.jsx';
+import cardData from './card_data.jsx';
 
 const App = () => {
+  // Set the card to be the starter card initially
+  const [content, setContent] = useState({question: "Start!", answer: "Press the 'next' button to start!"});
+
+  // Determine whether a card is flipped or not
+  const [flipped, setFlipped] = useState(false);
+
+  const newCard = () => {
+    let newIndex = Math.floor(Math.random() * cardData.length);
+    setContent(cardData[newIndex]);
+    setFlipped(false);
+  }
 
   // Return the contents of the page
   return (
     <div className='App'>
 
+      {/* Header for the app */}
       <div className='header'>
         <h1 >Learn Food in Spanish!</h1>
         <br />
@@ -15,6 +28,11 @@ const App = () => {
         <h2>Number of cards: { cardData.length }</h2>
       </div>
 
+      {/* Card with question and answer */}
+      <Card content={content} onClick={() => setFlipped(!flipped)}/>
+
+      {/* Button to go to the next card */}
+      <button onClick={newCard}>Next card</button>
 
     </div>
   )
